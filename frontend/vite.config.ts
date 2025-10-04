de1f5983d8182d023f84f5ce83a68ff0d91ff00b
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2025-10-04 22:23:06 +0700

    đổi cổng port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,       // Cho phép truy cập từ mạng LAN (nếu cần)
+    port: 3000,       // Đặt cổng mặc định là 3000
+    open: true,       // Tự động mở trình duyệt khi chạy
+  },
 })
