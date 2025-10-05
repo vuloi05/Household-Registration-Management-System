53cc62a5de7f508b9bd076f5dbe99984760c4631
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2025-10-05 14:59:18 +0700

    Xóa bảo mật

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -371,21 +371,8 @@ const Footer: React.FC = () => {
                       <rect x="2" y="4" rx="2" width="20" height="16" />
                     </g>
                   </svg>
-                  <span>info@infofuturist.vn</span>
+                  <span>manhtrana1k45tl@gmail.com</span>
                 </div>
-              </div>
-              <div className="footer-security">
-                <svg width="24" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24">
-                  <path
-                    d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-                <span>Bảo mật SSL 256-bit</span>
               </div>
             </div>
           </div>
