95b928ea628584980e71f20eb24a00d412d8bbdf
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2025-10-07 14:34:01 +0700

    thêm phần footer vào trang login

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import LoginSection from '../components/login/LoginSection';
 import Header from '../components/login/Header';
 
+
+import Footer from '../components/Footer';
+
 const Login: React.FC = () => {
   return (
     <>
       <Header />
       <LoginSection />
+      <div style={{ marginTop: '40px' }}>
+        <Footer />
+      </div>
     </>
   );
 };
