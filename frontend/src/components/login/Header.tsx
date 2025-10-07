46f672dcaab68d6cfcf1868a6ea2b242017c67b7
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2025-10-07 14:30:23 +0700

    fix căn giữa icon

--- frontend/src/components/login/Header.tsx
+++ frontend/src/components/login/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
           <img src="/login/avatar.png" alt="Avatar" width="80" height="80" />
         </div>
         <div className="header-text">
-          <h1>CỔNG DỊCH VỤ CÔNG QUỐC GIA</h1>
+          <h1>QUẢN LÝ THÔNG MINH</h1>
           <p>Kết nối, cung cấp thông tin và dịch vụ công mọi lúc, mọi nơi</p>
         </div>
       </div>
